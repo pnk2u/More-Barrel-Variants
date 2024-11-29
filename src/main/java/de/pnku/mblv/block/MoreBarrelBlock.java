b874453ee19343fd070e08b1c9e2a3915de954ce
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-11-29 02:09:03 +0100

    Initial Commit (1.21.4)

--- src/main/java/de/pnku/mblv/block/MoreBarrelBlock.java
+++ src/main/java/de/pnku/mblv/block/MoreBarrelBlock.java
@@ -15,7 +15,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BarrelBlock;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.entity.BarrelBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
